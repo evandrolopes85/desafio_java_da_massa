0c92ccd6d3749dc24db80d9eac4be853886a626d
evandrolopes85 <evandrocwd@gmail.com>
2021-07-20 16:36:21 -0300

    Removendo excessao do URI no metodo main

--- source/URI1020.java
+++ source/URI1020.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
  */
 public class URI1020 {
  
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
  
         Scanner teclado = new Scanner(System.in);
 
